3d1fda482e13a7faa9f3bace813816899ab96656
Samikshador5534 <paidlewarsamiksha@gmail.com>
2023-11-24 13:20:28 +0530

    project added

---
+++ src/com/mkpits/scopeofvariable/LocalVariableClass.java
@@ -0,0 +1,36 @@
+package com.mkpits.scopeofvariable;
+
+public class LocalVariableClass {
+	static int a=110;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// local variable declared inside any method
+		//we can not used local variable without initiallization
+		// we can not access that value from outside the method
+		
+		
+		int a=10;
+	    System.out.println(a);
+		System.out.println(LocalVariableClass.a);
+		SecondClassStatic sc=new SecondClassStatic();
+		System.out.println(sc.StaticVar);
+		
+
+		
+	}
+	
+	void add()
+	{
+		int pinCode=656868;
+		System.out.println(pinCode);
+	}
+	
+	void sub()
+	{
+		int pinCode=440023;
+		System.out.println(pinCode);
+	}
+
+}
---
+++ src/com/mkpits/scopeofvariable/StaticVariable.java
@@ -0,0 +1,15 @@
+package com.mkpits.scopeofvariable;
+
+public class StaticVariable {
+
+	 static int carprice = 3000000;
+	 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	System.out.println(StaticVariable.carprice);
+	
+
+}
+}
