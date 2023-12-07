0d2730e01004097dc0e3add3119e2721c8fc1e6a
Samikshador5534 <paidlewarsamiksha@gmail.com>
2023-12-07 14:23:35 +0530

    new file added

--- src/com/mkpits/oprators/Increment_Operator.java
+++ src/com/mkpits/oprators/Increment_Operator.java
@@ -11,6 +11,8 @@ public class Increment_Operator {
 
 	private void increment() {
 		int value = 50;
+		int b = 40;
+		
 		
 		System.out.println(++value);  //p - 51  memory - 51
 		System.out.println(++value);  //p - 52  memery - 52
@@ -18,6 +20,14 @@ public class Increment_Operator {
 		System.out.println(value++);  //p - 53  memory - 54
 		System.out.println(++value);  //p - 55  memory - 55
 		System.out.println(++value);  //p - 56  memory - 56
+		
+		
+		System.out.println(++b);  //p - 41  memory - 41
+		System.out.println(++b);  //p - 42  memery - 42
+		System.out.println(b++);  //p - 42  memory - 43
+		System.out.println(b++);  //p - 43  memory - 44
+		System.out.println(++b);  //p - 45  memory - 45
+		System.out.println(++b);  //p - 46  memory - 46
 		
 	}
 
