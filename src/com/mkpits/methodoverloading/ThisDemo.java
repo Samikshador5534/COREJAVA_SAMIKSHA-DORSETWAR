7b4ae6a4c001b88898ea2e7ba68b16b685c0abde
Samikshador5534 <paidlewarsamiksha@gmail.com>
2023-12-11 22:01:45 +0530

    new file added

--- src/com/mkpits/methodoverloading/ThisDemo.java
+++ src/com/mkpits/methodoverloading/ThisDemo.java
@@ -15,6 +15,20 @@ public class ThisDemo {
 		demo.code();
 		demo.mobileNo(45747375753l);
 		demo.getData(23,678);
+		System.out.println("                       ");
+		
+		
+		
+		System.out.println("this is the calling method of ThisDemoMethod class using getter n setter ");
+		ThisDemoMethod  method =new ThisDemoMethod("aatish", 2, 34637647365l, 'f');
+		System.out.println(method.getEmployeeName());
+		System.out.println(method.customerId);
+		System.out.println(method.mobileNo);
+		
+		
+		method.setEmployeeName("samiksha");
+		System.out.println(method.getEmployeeName());
+		
 	}
 
 	private void getData(int i, int j) {
