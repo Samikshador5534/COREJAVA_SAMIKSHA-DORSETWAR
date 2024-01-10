7ffc3cde85085f8ff0ac130ecbe315ca4655c2a8
Samikshador5534 <paidlewarsamiksha@gmail.com>
2024-01-10 12:06:25 +0530

    multiplication table

--- src/com/mkpits/array/Multiplication_Table.java
+++ src/com/mkpits/array/Multiplication_Table.java
@@ -1,15 +1,21 @@
 package com.mkpits.array;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Multiplication_Table {
 
-	 public static void main(String[] args) {
-	        int n = 11; // 
-	        
+	 public static void main(String[] args) throws NumberFormatException, IOException {
+	        int n = 50; // 
+	      //  BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	      //  System.out.print("enter N value : ");
+	      //  int n = Integer.parseInt(input.readLine());
 	        // Create a 2D array to store the multiplication table
-	        int[][] table = new int[n][n];
+	        int[][] table = new int[11][n];
 	        
 	        // for  multiplication table
-	        for (int i = 1; i < n; i++) {
+	        for (int i = 1; i < 11; i++) {
 	            for (int j = 1; j < n; j++) {
 	                table[i][j] = (i  * j );
 	            }
@@ -18,7 +24,8 @@ public class Multiplication_Table {
 	        // Display the multiplication table
 	        for (int i = 1; i < n; i++) {
 	            for (int j = 1; j < n; j++) {
-	                System.out.printf("%2d x %2d = %2d   ", i , j , table[i][j]);
+	               // System.out.printf("%2d x %2d = %2d   |", i , j , table[i][j]);
+	            	System.out.printf("  %2d  ",table[i][j]);
 	            }
 	            System.out.println(); 
 	        }
