479c815664c6dd4b9cf2ed58d539aa42340542b3
Samikshador5534 <paidlewarsamiksha@gmail.com>
2024-01-11 14:23:56 +0530

    goat interface method

--- src/com/mkpits/Interface/Goat.java
+++ src/com/mkpits/Interface/Goat.java
@@ -13,7 +13,7 @@ public class Goat implements Animal {
 	public void sleep() {
 		// TODO Auto-generated method stub
 		
-		System.out.println("shhhhh......");
+		System.out.println("shhhhhh......");
 	}
 
 }
