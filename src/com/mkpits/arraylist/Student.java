44be63021a2f343a17e9fcd6a46a9a9b89303b8d
Samikshador5534 <paidlewarsamiksha@gmail.com>
2024-01-17 18:02:57 +0530

    array list

---
+++ src/com/mkpits/arraylist/Student.java
@@ -0,0 +1,13 @@
+package com.mkpits.arraylist;
+
+public class Student {
+
+	String name;
+	Integer rollNo;
+	
+	public Student( String name , Integer rollNo) {
+		this.name =name;
+		this.rollNo=rollNo;
+	}
+	
+}
